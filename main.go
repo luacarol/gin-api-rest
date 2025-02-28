379ed8ae342947d66021705ee03349b773f960dd
Luana Caroliny <lucarolp.deoli@gmail.com>
2025-02-28 09:15:26 -0300

    feature: student string created

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/luacarol/api-go-gin/routes"
+import (
+	"github.com/luacarol/api-go-gin/models"
+	"github.com/luacarol/api-go-gin/routes"
+)
 
 func main() {
+	models.Alunos = []models.Aluno{
+		{Nome: "Luana dos Anjos", CPF: "00000000000", RG: "000000000"},
+		{Nome: "Matheus dos Anjos", CPF: "11111111111", RG: "111111111"},
+	}
 	routes.HandleRequests()
 }
