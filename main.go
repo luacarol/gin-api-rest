64ab2f7a4dc2d8e52a8d6db39ab9d757562bdb0e
Luana Caroliny <lucarolp.deoli@gmail.com>
2025-02-28 08:25:39 -0300

    refactor: controllers and routes created

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func ExibeTodosAlunos(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"id":   "1",
-		"nome": "Luana dos Anjos",
-	})
-}
+import "github.com/luacarol/api-go-gin/routes"
 
 func main() {
-	r := gin.Default()
-	r.GET("/alunos", ExibeTodosAlunos)
-	r.Run(":8080")
+	routes.HandleRequests()
 }
