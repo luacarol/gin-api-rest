ed1f499d5a16f0babc775372479d596a3f5a0124
Luana Caroliny <lucarolp.deoli@gmail.com>
2025-03-02 11:12:42 -0300

    feat: connecting api to server

---
+++ database/db.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/luacarol/api-go-gin/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+var err error
+
+func ConectaComBancoDeDados() {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Panic("Erro ao conectar com banco de dados")
+	}
+	DB.AutoMigrate(&models.Aluno{})
+}
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/luacarol/api-go-gin/database"
 	"github.com/luacarol/api-go-gin/models"
 	"github.com/luacarol/api-go-gin/routes"
 )
 
 func main() {
+	database.ConectaComBancoDeDados()
 	models.Alunos = []models.Aluno{
 		{Nome: "Luana dos Anjos", CPF: "00000000000", RG: "000000000"},
 		{Nome: "Matheus dos Anjos", CPF: "11111111111", RG: "111111111"},
